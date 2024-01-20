b5687e99e8048a3768cb775c18aa64f979d5d023
physicksKim <physicksKim@gmail.com>
2024-01-21 06:05:40 +0900

    refac : wrap setState props

--- src/components/control/ControlPanel.tsx
+++ src/components/control/ControlPanel.tsx
@@ -11,11 +11,11 @@ interface ControlPanelProps {
   disappearInjuryTimer: () => void;
   isShowInjuryTimer: boolean;
   updateGivenInjuryTime: (min: number) => void;
-  setMatchName: React.Dispatch<React.SetStateAction<string>>;
+  updateMatchName: (matchName: string) => void;
   teamA: Team;
   teamB: Team;
-  setTeamA: React.Dispatch<React.SetStateAction<Team>>;
-  setTeamB: React.Dispatch<React.SetStateAction<Team>>;
+  updateTeamA: <K extends keyof Team>(key: K, value: Team[K]) => void;
+  updateTeamB: <K extends keyof Team>(key: K, value: Team[K]) => void;
 }
 
 const ControlPanel: React.FC<ControlPanelProps> = ({
@@ -25,13 +25,17 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
   disappearInjuryTimer,
   isShowInjuryTimer,
   updateGivenInjuryTime,
-  setMatchName,
+  updateMatchName,
   teamA,
   teamB,
-  setTeamA,
-  setTeamB,
+  updateTeamA,
+  updateTeamB,
 }) => {
   return (
+    // TODO : epl 팀 추가하면 이제 control panel 이 변경될 수 있도록 해야함.
+    // 지금은 [타이머+팀선택] 으로 페이지 하나인데
+    // 나중에 [타이머] [팀선택] 으로 페이지 두 개 만들어야 할듯함
+    // ++ 팀선택 이전에 EPL 로고부터 해결해야 할듯?
     <div className='control-panel-container'>
       <div className='timer-control-wrapper'>
         <TimerControlPanel
@@ -41,15 +45,15 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
           disappearInjuryTimer={disappearInjuryTimer}
           isShowInjuryTimer={isShowInjuryTimer}
           updateGivenInjuryTime={updateGivenInjuryTime}
-          setMatchName={setMatchName}
+          updateMatchName={updateMatchName}
         />
       </div>
       <div className='board-control-wrapper'>
         <BoardControlPanel
           teamA={teamA}
           teamB={teamB}
-          setTeamA={setTeamA}
-          setTeamB={setTeamB}
+          updateTeamA={updateTeamA}
+          updateTeamB={updateTeamB}
         />
       </div>
     </div>
