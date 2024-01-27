7cff4be8cb95c3fc243d2cc6d7d86e0945cc9272
physicksKim <physicksKim@gmail.com>
2024-01-27 21:36:57 +0900

    style : adjust control panel css

--- src/components/TimerRoot.tsx
+++ src/components/TimerRoot.tsx
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  MemoryRouter,
-  HashRouter,
-} from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 
 import CSSTransition from 'react-transition-group/CSSTransition';
 
