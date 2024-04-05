f594f8c176b7a21d4723734ceef7524e1066b42f
physicksKim <physicksKim@gmail.com>
2024-04-05 20:46:21 +0900

    fixture : mun VS liv 32r

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -3,20 +3,20 @@ import EPL2324Codes from './EPLCodes';
 
 // '2023-24 잉글랜드 프리미어리그 30R';
 // '2023-24 잉글랜드 FA컵 4강'
-export const defaultMatchName = '2023-24 잉글랜드 프리미어리그 31R';
+export const defaultMatchName = '2023-24 잉글랜드 프리미어리그 32R';
 
 export const defaultTeamA: Team = {
   category: 'epl2324',
-  code: EPL2324Codes.맨시티.code,
-  name: EPL2324Codes.맨시티.name,
+  code: EPL2324Codes.맨유.code,
+  name: EPL2324Codes.맨유.name,
   score: 0,
   uniform: 'home',
 };
 
 export const defaultTeamB: Team = {
   category: 'epl2324',
-  code: EPL2324Codes.아스톤빌라.code,
-  name: EPL2324Codes.아스톤빌라.name,
+  code: EPL2324Codes.리버풀.code,
+  name: EPL2324Codes.리버풀.name,
   score: 0,
-  uniform: 'home',
+  uniform: 'away',
 };
