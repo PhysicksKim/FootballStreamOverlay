00764fc03576fdcc29f5f045f802056201cdd927
physicksKim <physicksKim@gmail.com>
2024-04-10 03:05:16 +0900

    fixture : modi mancity realmadrid unform and title

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -4,14 +4,14 @@ import EtcTeamCodes from './EtcTeamCodes';
 
 // '2023-24 잉글랜드 프리미어리그 30R';
 // '2023-24 잉글랜드 FA컵 4강'
-export const defaultMatchName = '2023-24 UEFA 챔피언스리그 8강';
+export const defaultMatchName = '2023-24 UEFA 챔피언스리그 8강 1차전';
 
 export const defaultTeamA: Team = {
   category: 'epl2324',
   code: EPL2324Codes.맨시티.code,
   name: EPL2324Codes.맨시티.name,
   score: 0,
-  uniform: 'home',
+  uniform: 'third',
 };
 
 export const defaultTeamB: Team = {
