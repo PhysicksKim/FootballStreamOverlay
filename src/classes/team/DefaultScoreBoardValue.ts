d2f6447b69f2b20a27c52473c3a246b169fd07e0
physicksKim <physicksKim@gmail.com>
2024-04-20 17:06:26 +0900

    fixture : fa cup semi final mancity vs chelsea

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -4,8 +4,9 @@ import EtcTeamCodes from './EtcTeamCodes';
 
 // '2023-24 잉글랜드 프리미어리그 30R';
 // '2023-24 잉글랜드 FA컵 4강'
+// '2023-24 UEFA 챔피언스리그 8강 2차전'
 
-export const defaultMatchName = '2023-24 UEFA 챔피언스리그 8강 2차전';
+export const defaultMatchName = '2023-24 잉글랜드 FA컵 4강';
 
 export const defaultTeamA: Team = {
   category: 'epl2324',
@@ -16,11 +17,11 @@ export const defaultTeamA: Team = {
 };
 
 export const defaultTeamB: Team = {
-  category: 'etc',
-  code: EtcTeamCodes.레알마드리드.code,
-  name: EtcTeamCodes.레알마드리드.name,
+  category: 'epl2324',
+  code: EPL2324Codes.첼시.code,
+  name: EPL2324Codes.첼시.name,
   score: 0,
-  uniform: 'third',
+  uniform: 'away',
 };
 
 // export const defaultMatchName = '2023-24 잉글랜드 프리미어리그 33R';
