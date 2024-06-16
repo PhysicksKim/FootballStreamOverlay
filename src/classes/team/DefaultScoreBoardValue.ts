8ccb1e411f3225805cade40a6eb273635f5e57bc
PhysicksKim <physickskim@gmail.com>
2024-06-16 14:29:39 +0900

    fixture : euro 2024 group c eng vs ser

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -5,23 +5,23 @@ import EtcTeamCodes from './EtcTeamCodes';
 
 export const defaultMatchNameTemplate = {
   EPL: '2023-24 잉글랜드 프리미어리그 38R',
-  EURO: 'UEFA 유로 2024 조별리그 B조',
+  EURO: 'UEFA 유로 2024 조별리그 C조',
 };
 
 export const defaultMatchName = defaultMatchNameTemplate.EURO;
 
 export const defaultTeamA: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.스페인.code,
-  name: Euro2024Codes.스페인.name,
+  code: Euro2024Codes.잉글랜드.code,
+  name: Euro2024Codes.잉글랜드.name,
   score: 0,
   uniform: 'home',
 };
 
 export const defaultTeamB: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.크로아티아.code,
-  name: Euro2024Codes.크로아티아.name,
+  code: Euro2024Codes.세르비아.code,
+  name: Euro2024Codes.세르비아.name,
   score: 0,
-  uniform: 'away',
+  uniform: 'home',
 };
