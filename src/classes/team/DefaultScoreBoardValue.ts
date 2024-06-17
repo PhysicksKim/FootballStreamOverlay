f6cadb6bef892f05def21e6b3f82f08b43e94e1e
PhysicksKim <physickskim@gmail.com>
2024-06-17 18:14:52 +0900

    fixture : euro 2024 por vs cze

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -5,23 +5,23 @@ import EtcTeamCodes from './EtcTeamCodes';
 
 export const defaultMatchNameTemplate = {
   EPL: '2023-24 잉글랜드 프리미어리그 38R',
-  EURO: 'UEFA 유로 2024 조별리그 C조',
+  EURO: 'UEFA 유로 2024 조별리그 F조',
 };
 
 export const defaultMatchName = defaultMatchNameTemplate.EURO;
 
 export const defaultTeamA: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.잉글랜드.code,
-  name: Euro2024Codes.잉글랜드.name,
+  code: Euro2024Codes.포르투갈.code,
+  name: Euro2024Codes.포르투갈.name,
   score: 0,
   uniform: 'home',
 };
 
 export const defaultTeamB: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.세르비아.code,
-  name: Euro2024Codes.세르비아.name,
+  code: Euro2024Codes.체코.code,
+  name: Euro2024Codes.체코.name,
   score: 0,
-  uniform: 'home',
+  uniform: 'away',
 };
