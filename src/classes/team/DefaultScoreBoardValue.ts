f94c5dad7f96629110290e8e0baad2bf72ab4b78
PhysicksKim <physickskim@gmail.com>
2024-07-07 22:47:34 +0900

    fixture : euro 2024 semi final esp vs fra

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -5,23 +5,23 @@ import EtcTeamCodes from './EtcTeamCodes';
 
 export const defaultMatchNameTemplate = {
   EPL: '2023-24 잉글랜드 프리미어리그 38R',
-  EURO: 'UEFA 유로 2024 토너먼트 8강',
+  EURO: 'UEFA 유로 2024 토너먼트 4강',
 };
 
 export const defaultMatchName = defaultMatchNameTemplate.EURO;
 
 export const defaultTeamA: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.잉글랜드.code,
-  name: Euro2024Codes.잉글랜드.name,
+  code: Euro2024Codes.스페인.code,
+  name: Euro2024Codes.스페인.name,
   score: 0,
   uniform: 'home',
 };
 
 export const defaultTeamB: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.스위스.code,
-  name: Euro2024Codes.스위스.name,
+  code: Euro2024Codes.프랑스.code,
+  name: Euro2024Codes.프랑스.name,
   score: 0,
   uniform: 'home',
 };
