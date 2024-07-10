605384586ee501ae4a756d6efcd0c63baaefe4a2
PhysicksKim <physickskim@gmail.com>
2024-07-10 16:12:37 +0900

    fixture : euro semi final ned vs eng

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -12,16 +12,16 @@ export const defaultMatchName = defaultMatchNameTemplate.EURO;
 
 export const defaultTeamA: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.스페인.code,
-  name: Euro2024Codes.스페인.name,
+  code: Euro2024Codes.네덜란드.code,
+  name: Euro2024Codes.네덜란드.name,
   score: 0,
   uniform: 'home',
 };
 
 export const defaultTeamB: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.프랑스.code,
-  name: Euro2024Codes.프랑스.name,
+  code: Euro2024Codes.잉글랜드.code,
+  name: Euro2024Codes.잉글랜드.name,
   score: 0,
   uniform: 'home',
 };
