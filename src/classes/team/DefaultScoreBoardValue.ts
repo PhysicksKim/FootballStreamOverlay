41a6b3090570373e1497bb7113efea56df933d34
PhysicksKim <physickskim@gmail.com>
2024-07-12 15:34:39 +0900

    fixture : euro 2024 final esp vs eng

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -5,15 +5,15 @@ import EtcTeamCodes from './EtcTeamCodes';
 
 export const defaultMatchNameTemplate = {
   EPL: '2023-24 잉글랜드 프리미어리그 38R',
-  EURO: 'UEFA 유로 2024 토너먼트 4강',
+  EURO: 'UEFA 유로 2024 결승전',
 };
 
 export const defaultMatchName = defaultMatchNameTemplate.EURO;
 
 export const defaultTeamA: Team = {
   category: 'euro2024',
-  code: Euro2024Codes.네덜란드.code,
-  name: Euro2024Codes.네덜란드.name,
+  code: Euro2024Codes.스페인.code,
+  name: Euro2024Codes.스페인.name,
   score: 0,
   uniform: 'home',
 };
