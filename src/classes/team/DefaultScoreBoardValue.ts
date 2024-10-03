d3fe3d47a83d47a6f69a61a02106d997833c832b
PhysicksKim <physickskim@gmail.com>
2024-10-04 04:33:50 +0900

    fixture : epl 2425 7R mci vs ful

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -4,7 +4,7 @@ import Euro2024Codes from './Euro2024Codes';
 import EtcTeamCodes from './EtcTeamCodes';
 
 export const defaultMatchNameTemplate = {
-  EPL: '2024-25 잉글랜드 프리미어리그 6R',
+  EPL: '2024-25 잉글랜드 프리미어리그 7R',
   EURO: 'UEFA EURO 2024',
   ETC: '2024-25 FA 커뮤니티 실드',
 };
@@ -13,16 +13,16 @@ export const defaultMatchName = defaultMatchNameTemplate.EPL;
 
 export const defaultTeamA: Team = {
   category: 'epl2324',
-  code: EPL2324Codes.맨유.code,
-  name: EPL2324Codes.맨유.name,
+  code: EPL2324Codes.맨시티.code,
+  name: EPL2324Codes.맨시티.name,
   score: 0,
   uniform: 'home',
 };
 
 export const defaultTeamB: Team = {
   category: 'epl2324',
-  code: EPL2324Codes.토트넘.code,
-  name: EPL2324Codes.토트넘.name,
+  code: EPL2324Codes.풀럼.code,
+  name: EPL2324Codes.풀럼.name,
   score: 0,
   uniform: 'home',
 };
