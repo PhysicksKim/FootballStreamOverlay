215c2c83ed15f80044b8287d509c443edb1ac925
PhysicksKim <physickskim@gmail.com>
2024-10-20 02:58:24 +0900

    fixture: epl 2425 8R mancity vs wolves

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -4,7 +4,7 @@ import Euro2024Codes from './Euro2024Codes';
 import EtcTeamCodes from './EtcTeamCodes';
 
 export const defaultMatchNameTemplate = {
-  EPL: '2024-25 잉글랜드 프리미어리그 7R',
+  EPL: '2024-25 잉글랜드 프리미어리그 8R',
   EURO: 'UEFA EURO 2024',
   ETC: '2024-25 FA 커뮤니티 실드',
 };
@@ -21,8 +21,8 @@ export const defaultTeamA: Team = {
 
 export const defaultTeamB: Team = {
   category: 'epl2324',
-  code: EPL2324Codes.풀럼.code,
-  name: EPL2324Codes.풀럼.name,
+  code: EPL2324Codes.울버햄튼.code,
+  name: EPL2324Codes.울버햄튼.name,
   score: 0,
   uniform: 'home',
 };
