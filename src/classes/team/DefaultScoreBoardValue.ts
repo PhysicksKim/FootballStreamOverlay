aff3f3644b9b62843ac12a79557943abf03590ee
PhysicksKim <physickskim@gmail.com>
2024-11-09 13:15:41 +0900

    fixture : epl 2425 11R mci vs bri

--- src/classes/team/DefaultScoreBoardValue.ts
+++ src/classes/team/DefaultScoreBoardValue.ts
@@ -4,7 +4,7 @@ import Euro2024Codes from './Euro2024Codes';
 import EtcTeamCodes from './EtcTeamCodes';
 
 export const defaultMatchNameTemplate = {
-  EPL: '2024-25 잉글랜드 프리미어리그 10R',
+  EPL: '2024-25 잉글랜드 프리미어리그 11R',
   EURO: 'UEFA EURO 2024',
   ETC: '2024-25 FA 커뮤니티 실드',
 };
@@ -13,16 +13,16 @@ export const defaultMatchName = defaultMatchNameTemplate.EPL;
 
 export const defaultTeamA: Team = {
   category: 'epl2324',
-  code: EPL2324Codes.맨유.code,
-  name: EPL2324Codes.맨유.name,
+  code: EPL2324Codes.맨시티.code,
+  name: EPL2324Codes.맨시티.name,
   score: 0,
   uniform: 'home',
 };
 
 export const defaultTeamB: Team = {
   category: 'epl2324',
-  code: EPL2324Codes.첼시.code,
-  name: EPL2324Codes.첼시.name,
+  code: EPL2324Codes.브라이튼.code,
+  name: EPL2324Codes.브라이튼.name,
   score: 0,
   uniform: 'home',
 };
